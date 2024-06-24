18071a22be4cbd357a7a89a7d2d80c2597601a03
Lorena Garcia <lorenaamesqueta@gmail.com>
2024-06-23 19:02:49 -0500

    subiendo avance pelicula detalle
    deadpool

--- src/pelicula-detalle.js
+++ src/pelicula-detalle.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    peliculaDetalle();
+    
+});
+
+
+function peliculaDetalle(){
+    viewTrailer();
+    selectDate();
+}
+
+
+function selectDate(){
     const buttons = document.querySelectorAll('.btn-date');
 
     buttons.forEach(button => {
@@ -7,4 +20,37 @@ document.addEventListener('DOMContentLoaded', function() {
             this.classList.add('active');
         });
     });
-});
+}
+ 
+function onPlayerStateChange(event) {
+    const detailMovieContainer = document.getElementById("detailMovieContainer");
+    const videoContainer = document.getElementById("videoContainer");
+    
+    if (event.data == YT.PlayerState.ENDED) {
+        videoContainer.classList.add("d-none");
+        detailMovieContainer.classList.remove("d-none");
+    }
+}
+
+function viewTrailer(){
+
+    player = new YT.Player('trailerVideo', {
+        events: {
+            'onStateChange': onPlayerStateChange
+        }
+    });
+
+    const detailMovieContainer = document.getElementById("detailMovieContainer");
+    const viewTrailerButton = document.getElementById("viewTrailerButton");
+    const videoContainer = document.getElementById("videoContainer");
+
+    viewTrailerButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        
+        videoContainer.classList.remove("d-none");
+        detailMovieContainer.classList.add("d-none");
+
+        // Reproducir el video automáticamente
+        player.playVideo();
+    });
+}
