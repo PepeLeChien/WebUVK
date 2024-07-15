58566895c512b13a4cb2b3a85543d4b4df38d5a2
Juan Lozada <juanlozadacochaches@gmail.com>
2024-07-15 16:11:55 -0500

    -

--- mvc/config/config.php
+++ mvc/config/config.php
@@ -1,11 +1,11 @@
 <?php
-// define('URL', 'http://localhost:3000/');
-// define('HOST', 'monorail.proxy.rlwy.net');
-// define('DB', 'DbCine');
-// define('USER', 'root');
-// define('PASSWORD', 'gLiViqcicZjbKSsXiJTkXyuhwkHlxNXk');
-// define('PORT', 58970);
-// define('CHARSET', 'utf8mb4');
+define('URL', 'http://localhost:3000/');
+define('HOST', 'monorail.proxy.rlwy.net');
+define('DB', 'DbCine');
+define('USER', 'root');
+define('PASSWORD', 'gLiViqcicZjbKSsXiJTkXyuhwkHlxNXk');
+define('PORT', 58970);
+define('CHARSET', 'utf8mb4');
 
 // define('URL', 'http://localhost:3000/');
 // define('HOST', 'db-projects.cn62022agaif.us-east-2.rds.amazonaws.com');
@@ -15,11 +15,11 @@
 // define('PORT', 3306);
 // define('CHARSET', 'utf8mb4');
 
-define('URL', 'http://localhost:3000/');
-define('HOST', 'localhost');
-define('DB', 'DbCine');
-define('USER', 'root');
-define('PASSWORD', '12345');
-define('PORT', 3306);
-define('CHARSET', 'utf8mb4');
+// define('URL', 'http://localhost:3000/');
+// define('HOST', 'localhost');
+// define('DB', 'DbCine');
+// define('USER', 'root');
+// define('PASSWORD', '12345');
+// define('PORT', 3306);
+// define('CHARSET', 'utf8mb4');
 ?>
